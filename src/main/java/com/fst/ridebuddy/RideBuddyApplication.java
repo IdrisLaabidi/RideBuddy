575c74e2172ba7331426c11db8111904bf3b2abe
IdrisLaabidi <idrislaabidi@gmail.com>
2024-11-23 19:00:29 +0100

    Initial commit

---
+++ src/main/java/com/fst/ridebuddy/RideBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.fst.ridebuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideBuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fst/ridebuddy/RideBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fst.ridebuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

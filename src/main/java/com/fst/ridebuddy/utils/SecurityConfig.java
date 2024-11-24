3afe419765be6cdf7341d8ef21b3b3ff1a364ce2
IdrisLaabidi <idrislaabidi@gmail.com>
2024-11-24 17:38:33 +0100

    completed contact.html

--- src/main/java/com/fst/ridebuddy/utils/SecurityConfig.java
+++ src/main/java/com/fst/ridebuddy/utils/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.fst.ridebuddy.utils;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,22 +13,24 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .authorizeHttpRequests ( auth -> auth
-                        . requestMatchers("/").permitAll ()
-                        . requestMatchers("/contact").permitAll ()
-                        .requestMatchers("/rides/ ** ").permitAll ()
-                        . requestMatchers("/register").permitAll ()
-                        . requestMatchers("/login").permitAll ()
-                        .requestMatchers("/logout").permitAll ()
-                        . anyRequest ().authenticated ()
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/").permitAll()
+                        .requestMatchers("/contact").permitAll()
+                        .requestMatchers("/rides/**").permitAll()
+                        .requestMatchers("/register").permitAll()
+                        .requestMatchers("/login").permitAll()
+                        .requestMatchers("/logout").permitAll()
+                        .anyRequest().authenticated() // Any other request requires authentication
                 )
-                .formLogin (form -> form
-                        .defaultSuccessUrl ("/", true)
+                .formLogin(form -> form
+                        .defaultSuccessUrl("/", true) // Redirect to homepage after successful login
                 )
-                .logout (config -> config.logoutSuccessUrl ("/"))
-                .build ();
+                .logout(config -> config
+                        .logoutSuccessUrl("/") // Redirect to homepage after logout
+                )
+                .build();
     }
 
     @Bean

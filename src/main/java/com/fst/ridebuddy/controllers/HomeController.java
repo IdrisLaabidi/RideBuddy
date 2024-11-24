68fb57cf5227001c6e62b9e103e14d4f9d8f66b4
IdrisLaabidi <idrislaabidi@gmail.com>
2024-11-24 21:40:17 +0100

    you can now change your profile picture

--- src/main/java/com/fst/ridebuddy/controllers/HomeController.java
+++ src/main/java/com/fst/ridebuddy/controllers/HomeController.java
@@ -1,15 +1,27 @@
 package com.fst.ridebuddy.controllers;
 
+import com.fst.ridebuddy.entities.AppUser;
+import com.fst.ridebuddy.services.AppUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    private AppUserService userService;
+
     @GetMapping({"","/"})
-    public String home () {
+    public String home(Model model) {
+        AppUser user = userService.getAuthenticatedUser();
+        if (user != null) {
+            model.addAttribute("user", user);
+        }
         return "index";
     }
+
     @GetMapping("/contact")
     public String contact () {
         return "contact";
